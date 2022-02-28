2f49572dca3c6a262183ad138599bd47c8213008
Colby <colby.sites@gmail.com>
2022-02-27 20:14:04 -0600

    Removing packages.

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='audio_to_visualization',
       version='1.0',
       description='Audio to visualization script',
       author='CodingVibe',
-      url='https://github.com/codingvibe/audio-to-vizualization',
-      packages=['ffmpeg_python'],
+      url='https://github.com/codingvibe/audio-to-vizualization'
      )
