a7e324ddd714738b96a130490bc4eeb51f36ed19
Colby <colby.sites@gmail.com>
2022-02-27 20:08:28 -0600

    Adding setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='audio_to_visualization',
+      version='1.0',
+      description='Audio to visualization script',
+      author='CodingVibe',
+      url='https://github.com/codingvibe/audio-to-vizualization',
+      packages=['ffmpeg_python'],
+     )
